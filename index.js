41bb231bcd1c38b2c36ce84d3a36f401315edde6
wbaumgardner <wbaumgardner@ucdavis.edu>
2020-01-18 20:02:35 -0800

    hi

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const mysql = require('mysql')
+const fs = require('fs')
+
 const app = express()
 var expressWs = require('express-ws')(app);
 const exphbs = require('express-handlebars');
 const port = 3000
+const VideoSavePath = '/ '
 
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
@@ -13,37 +17,44 @@ app.get('/', function(a_req, a_resp) {
 })
 
 // Watch page
-app.get('/watch/\d+', function(a_req, a_resp) {
+app.get('/watch/+', function(a_req, a_resp) {
 	console.log(a_req.path)
 	a_resp.render('watch', {"videoID": "abcd"})
 })
 
 // returns accessToken
-app.post('/Upload', function(a_req, a_resp)){
+app.post('/Upload', function(a_req, a_resp){
     var sessionPword = a_req.param('password');
-    //todo: add file
+    var fileData = a_req.param('file')
+    var fileName =  Math.random()+ ".mp4"
+    i
+    fs.writeFile(filePath + fileName, fileData,  (error) =>
+    if(error)
+    {
+      return;
+      throw error;
 
-
+    })
 })
+
+
 // request host access  
 app.post('/RequestWater', function(a_req, a_resp){
   var videoId  = a_req.param('videoID')
   var password = a_req.param('password')
 
-
-
 })
 
-app.get('/WaterTowerTime', function(a_req, a_resp)){
-  constant resp = {"Status":"Success", "Result":{"timeStamp": Date.now()}}
-  response.json(resp)
-
+app.get('/WaterTowerTime', function(a_req, a_resp){
+  var resp = {"timeStamp": Date.now()}
+  console.log(resp);
+  a_resp.json(resp)
 })
 
 //
 app.ws('/faucet', function(a_ws, a_req) {
 	ws.on('message', function(msg) {
-		
+
 		ws.send(msg);
 	});
 });
